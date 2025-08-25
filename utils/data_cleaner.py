9e971623082cae73f15ab26735838b4c625588ba
YangFuHua <fuhuayang899@gmail.com>
2025-08-25 15:48:19 +0800

    设置Pandas版本号

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -7,7 +7,7 @@ from loguru import logger
 from utils.monitor import execution_monitor
 
 try:
-    if pd.__version__ <= '2.2.0':
+    if pd.__version__ < '2.2.0':
         warnings.warn("pandas 2.2.0 及以下版本存在向下转换问题，建议升级到2.3.0及以上版本以避免潜在问题。", UserWarning)
     pd.set_option('future.no_silent_downcasting', True)
 except Exception as e:
